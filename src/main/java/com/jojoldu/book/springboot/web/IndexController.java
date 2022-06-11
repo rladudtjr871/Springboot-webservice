cc423e103ac4b867b3c0a56d6953906063205592
rladudtjr871 <rladudtjr871@naver.com>
2022-06-11 23:07:33 +0900

    5.4 & 5.5 - 어노테이션 기반으로 개선, 세션 저장소로 데이터베이스 사용하기

--- src/main/java/com/jojoldu/book/springboot/web/IndexController.java
+++ src/main/java/com/jojoldu/book/springboot/web/IndexController.java
@@ -1,5 +1,6 @@
 package com.jojoldu.book.springboot.web;
 
+import com.jojoldu.book.springboot.config.auth.LoginUser;
 import com.jojoldu.book.springboot.config.auth.dto.SessionUser;
 import com.jojoldu.book.springboot.service.posts.PostsService;
 import com.jojoldu.book.springboot.web.dto.PostsResponseDto;
@@ -9,14 +10,12 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-import javax.servlet.http.HttpSession;
-
 @RequiredArgsConstructor
 @Controller
 public class IndexController {
 
     private final PostsService postsService;
-    private final HttpSession httpSession;
+    //private final HttpSession httpSession;
 
     @GetMapping
     public String index() {
@@ -29,13 +28,17 @@ public class IndexController {
     }
 
     @GetMapping("/")
-    public String index(Model model) { //서버 템플릿 엔진에서 사용할 수 있는 객체를 저장할 수 있다.
-                        // 여기서는 postsService.findAllDesc()로 가져온 결과를  posts로 index.mustache에 전달한다.
+    public String index(Model model, @LoginUser SessionUser user) { //서버 템플릿 엔진에서 사용할 수 있는 객체를 저장할 수 있다.
+        // 여기서는 postsService.findAllDesc()로 가져온 결과를  posts로 index.mustache에 전달한다.
+        // 199p >개선: @LoginUser SessionUser > 기존에 httpSession.getAttribute("user")로 가져오던 세션 정보 값이 개선
+        //이제 어느 컨트롤러든 @LoginUser만 사용하면 세션 정보를 가져올 수 있게 되었다.
+
+
         model.addAttribute("posts", postsService.findAllDesc());
         
         //앞서 작성된 CustomOAuth2UserService에서 로그인 성공시 세션에 SessionUser를 저장하도록 구성
         //즉, 로그인 성공시 httpSession.getAttribute("user")에서 값을 가져올 수 있다.
-        SessionUser user = (SessionUser) httpSession.getAttribute("user");
+        //SessionUser user = (SessionUser) httpSession.getAttribute("user");
         
         //세션에 저장된 값이 있으면 model에 userName으로 등록
         if (user != null) {
