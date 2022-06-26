57013f864e6f065496cf31e91889ae4923da1931
rladudtjr871 <rladudtjr871@naver.com>
2022-06-26 13:28:06 +0900

    "10.3 - 무중단 배포 스크립트 만들기"

--- src/main/java/com/jojoldu/book/springboot/config/auth/SecurityConfig.java
+++ src/main/java/com/jojoldu/book/springboot/config/auth/SecurityConfig.java
@@ -18,7 +18,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .csrf().disable().headers().frameOptions().disable() //h2-console 화면을 사용하기 위해 해당 옵션들을 disable한다.
                 .and()
                     .authorizeRequests()    //URL별 권한 관리를 설정하는 옵션의 시작점 > 이게 선언되어야 antMatchers 옵션 사용가능
-                    .antMatchers("/", "/css/**", "/images/**", "/js/**", "/h2-console/**")
+                    .antMatchers("/", "/css/**", "/images/**", "/js/**", "/h2-console/**", "/profile")
                     .permitAll()
                     .antMatchers("/api/v1/**").hasRole(Role.USER.name()) //권한 관리 대상을 지정하는 옵션 > "/"등 지정된 URL들은 permitAll() 옵션을 통해 전체 열람권한을 줌ㅁ
                                                                          //"/api/v1/**"주소를 가진 API는 USER 권한을 가진 사람만 가능하도록 했다.
