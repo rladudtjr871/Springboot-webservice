f89de94771dca325e4562c942236a65192c3a1bb
rladudtjr871 <rladudtjr871@naver.com>
2022-06-26 14:09:45 +0900

    "10.3 - 무중단 배포 스크립트 만들기"

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -11,9 +11,10 @@ echo "> 현재 구동 중인 pid 확인"
 
 CURRENT_PID=$(pgrep -fl $PROJECT_NAME | grep java | awk '{print $1}')
 
+
 echo "현재 구동 중인 애플리케이션 pid: $CURRENT_PID"
 
-if [ -z "$CURRENT_PID"]; then
+if [ -z "$CURRENT_PID" ]; then
   echo "> 현재 구동 중인 애플리케이션이 없으므로 종료하지 않습니다."
 else
   echo "> kill -15 $CURRENT_PID"
@@ -32,7 +33,7 @@ echo "> $JAR_NAME에 실행 권한 추가"
 
 chmod +x $JAR_NAME
 
-ehco "> $JAR_NAME 실행"
+echo "> $JAR_NAME 실행"
 
 nohup java -jar \
   -Dspring.config.location=classpath:/application.properties,classpath:/application-real.properties,/home/ec2-user/app/application-oauth.properties,/home/ec2-user/app/application-real-db.properties \
